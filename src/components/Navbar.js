a8e173f22e745163f917cc1145ff2b99d2655146
SabhyaSingh93 <sabhyasiet087@gmail.com>
2022-04-18 00:26:42 +0530

    2 commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,63 @@
+/* eslint-disable react/style-prop-object */
+
+/* eslint-disable react/no-typos */
+
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function Navbar(props) {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo03"
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <a className="navbar-brand" href="/">
+          {props.title}
+        </a>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                {props.aboutText}
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
+Navbar.PropTypes = { title: PropTypes.string, aboutText: PropTypes.string };
+
+Navbar.defaultProps = {
+  title: "set title here",
+  aboutText: "About",
+};
---
+++ src/components/TextForm.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const HandleonChange = (event) => {
+    console.log("on change");
+    setText(event.target.value);
+  };
+
+  const ChangeUpperCase = () => {
+    setText(text.toUpperCase());
+  };
+
+  return (
+    <>
+      <div className="container mt-3">
+        <div className=" mb-3">
+          <h2>{props.heading}</h2>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={HandleonChange}
+            id="exampleFormControlTextarea1"
+            rows="10"
+          ></textarea>
+        </div>
+        <div className="btn btn-primary" onClick={ChangeUpperCase}>
+          {" "}
+          Change to UpperCase
+        </div>
+      </div>
+    </>
+  );
+}
