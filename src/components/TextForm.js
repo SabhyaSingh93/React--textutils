8a6de630fc62546300b388501f6f7ee43152ba03
SabhyaSingh93 <sabhyasiet087@gmail.com>
2022-04-23 21:49:40 +0530

    textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,24 @@ export default function TextForm(props) {
     let newText = text.toUpperCase();
     setText(newText);
   };
+  const ChangeLowerCase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const ClearText = () => {
+    let newText = "";
+    setText(newText);
+  };
+  const RemoveExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  const CopyText = () => {
+    var text = document.getElementById("textBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
 
   return (
     <>
@@ -24,13 +42,48 @@ export default function TextForm(props) {
             className="form-control"
             value={text}
             onChange={HandleonChange}
-            id="exampleFormControlTextarea1"
+            id="textBox"
             rows="10"
+            placeholder="write text here."
           ></textarea>
         </div>
         <div className="btn btn-primary" onClick={ChangeUpperCase}>
-          {" "}
           Change to UpperCase
+        </div>
+        <div className="btn btn-primary mx-1" onClick={ChangeLowerCase}>
+          Change to LowerCase
+        </div>
+
+        <div className="btn btn-primary mx-1" onClick={ClearText}>
+          Clear text
+        </div>
+
+        <div className="btn btn-primary mx-1" onClick={CopyText}>
+          Copy Text
+        </div>
+        <div className="container card border-info mb-3 my-3">
+          <h2>Your Text Summary</h2>
+          <p>
+            <b>Characters : </b>
+            {text.trim().length}{" "}
+          </p>
+          <p>
+            <b>Words : </b>
+            {
+              text
+                .replace(/\n/g, " ")
+                .split(" ")
+                .filter((value) => value !== "").length
+            }{" "}
+          </p>
+          <p>
+            <b>No of Sentences : </b>
+            {text.split(/[".!?"]/).length - 1}{" "}
+          </p>
+        </div>
+        <div className="container card border-info mb-3 my-3">
+          <h2>Preview</h2>
+          <p>{text}</p>
         </div>
       </div>
     </>
